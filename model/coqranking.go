4057053e5e9eef67cdfeef64ca6384e14abede8d
Movilogica <bveracastellanos@gmail.com>
2024-05-27 15:40:41 -0700

    Ranking & Sorteo

---
+++ model/coqranking.go
@@ -0,0 +1,117 @@
+package model
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+)
+
+// Ranking
+type CoqRankingE struct {
+	Orden        NullInt32  `json:"orden,omitempty"`
+	Campeonatoid NullInt64  `json:"campeonato_id,omitempty"`
+	Galponid     NullInt64  `json:"galpon_id,omitempty"`
+	GalponText   NullString `json:"galpon_nombre,omitempty"`
+	Pendientes   NullInt32  `json:"pendientes,omitempty"`
+	Finalizadas  NullInt32  `json:"finalizadas,omitempty"`
+	Ganadas      NullInt32  `json:"ganadas,omitempty"`
+	Empatadas    NullInt32  `json:"empatadas,omitempty"`
+	Perdidas     NullInt32  `json:"perdidas,omitempty"`
+	Tiempo       NullInt32  `json:"tiempo,omitempty"`
+	Total        NullInt32  `json:"total,omitempty"`
+	Ruf1         NullString `json:"ruf1,omitempty"`
+	Ruf2         NullString `json:"ruf2,omitempty"`
+	Ruf3         NullString `json:"ruf3,omitempty"`
+	Iv           NullString `json:"iv,omitempty"`
+	Salt         NullString `json:"salt,omitempty"`
+	Checksum     NullString `json:"checksum,omitempty"`
+	FCreated     NullTime   `json:"fcreated,omitempty"`
+	FUpdated     NullTime   `json:"fupdated,omitempty"`
+	Activo       int32      `json:"activo,omitempty"`
+	Estadoreg    int32      `json:"estadoreg,omitempty"`
+	TotalRecords int64      `json:"total_records,omitempty"`
+}
+
+func (e CoqRankingE) MarshalJSON() ([]byte, error) {
+	return MarshalJSON_Not_Nulls(e)
+}
+
+const querySelectCoqRanking = `select * from coq_ranking_peleas_list( $1, $2)`
+
+//---------------------------------------------------------------------
+//MySQL               PostgreSQL            Oracle
+//=====               ==========            ======
+//WHERE col = ?       WHERE col = $1        WHERE col = :col
+//VALUES(?, ?, ?)     VALUES($1, $2, $3)    VALUES(:val1, :val2, :val3)
+//---------------------------------------------------------------------
+
+// GetAll returns a slice of all users, sorted by last name
+func (u *CoqRankingE) GetAll(token string, filter string) ([]*CoqRankingE, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := querySelectCoqRanking
+
+	// Se deseenvuelve el JSON del Filter para adicionar filtros
+	var mapFilter map[string]interface{}
+	json.Unmarshal([]byte(filter), &mapFilter)
+	if mapFilter == nil {
+		mapFilter = make(map[string]interface{})
+	}
+	// --- Adicion de filtros
+	// mapFilter["tipo"] = tabla
+	// Se empaqueta el JSON del Filter
+	jsonFilter, err := json.Marshal(mapFilter)
+	if err != nil {
+		log.Println("Error convirtiendo el Filter")
+	}
+	log.Println("Where = " + string(jsonFilter))
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	rows, err := stmt.QueryContext(ctx, token, string(jsonFilter))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var lista []*CoqRankingE
+
+	for rows.Next() {
+		var rowdata CoqRankingE
+		err := rows.Scan(
+			&rowdata.Orden,
+			&rowdata.Campeonatoid,
+			&rowdata.Galponid,
+			&rowdata.GalponText,
+			&rowdata.Pendientes,
+			&rowdata.Finalizadas,
+			&rowdata.Ganadas,
+			&rowdata.Empatadas,
+			&rowdata.Perdidas,
+			&rowdata.Tiempo,
+			&rowdata.Total,
+			/*&rowdata.Ruf1,
+			&rowdata.Ruf2,
+			&rowdata.Ruf3,
+			&rowdata.Iv,
+			&rowdata.Salt,
+			&rowdata.Checksum,
+			&rowdata.FCreated,
+			&rowdata.FUpdated,
+			&rowdata.Activo,
+			&rowdata.Estadoreg,*/
+			&rowdata.TotalRecords,
+		)
+		if err != nil {
+			log.Println("Error scanning", err)
+			return nil, err
+		}
+
+		lista = append(lista, &rowdata)
+	}
+
+	return lista, nil
+}
---
+++ model/coqsorteo.go
@@ -0,0 +1,317 @@
+package model
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+// Sorteo
+type CoqSorteoE struct {
+	Uniqueid                   int64      `json:"uniqueid,omitempty"`
+	Owner                      NullInt32  `json:"owner,omitempty"`
+	Dispositivoid              NullInt32  `json:"dispositivoid,omitempty"`
+	Id                         int32      `json:"id,omitempty"`
+	Sede                       int32      `json:"sede"`
+	Flag1                      string     `json:"flag1,omitempty"`
+	Flag2                      string     `json:"flag2,omitempty"`
+	CountryCode                NullString `json:"countrycode,omitempty"`
+	Campeonatoid               NullInt64  `json:"campeonato_id,omitempty"`
+	CondicionarPeso            NullInt32  `json:"condicionarpeso,omitempty"`
+	NoPelearMismoGalpon        NullInt32  `json:"nopelearmismogalpon,omitempty"`
+	NoPelearMismoCriador       NullInt32  `json:"nopelearmismocriador,omitempty"`
+	PermitirPeleasMismoGrupo   NullInt32  `json:"permitirpeleasmismogrupo,omitempty"`
+	LimiteMaxDiffPeso          NullInt32  `json:"limitemaxdiffpeso,omitempty"`
+	PermitirGrabarConRezagados NullInt32  `json:"permitirgrabarsorteoconrezagados,omitempty"`
+	SecuenciaSorteo            NullInt32  `json:"sorteosecuencial,omitempty"`
+	Ruf1                       NullString `json:"ruf1,omitempty"`
+	Ruf2                       NullString `json:"ruf2,omitempty"`
+	Ruf3                       NullString `json:"ruf3,omitempty"`
+	Iv                         NullString `json:"iv,omitempty"`
+	Salt                       NullString `json:"salt,omitempty"`
+	Checksum                   NullString `json:"checksum,omitempty"`
+	FCreated                   NullTime   `json:"fcreated,omitempty"`
+	FUpdated                   NullTime   `json:"fupdated,omitempty"`
+	Activo                     int32      `json:"activo,omitempty"`
+	Estadoreg                  int32      `json:"estadoreg,omitempty"`
+	TotalRecords               int64      `json:"total_records,omitempty"`
+}
+
+func (e CoqSorteoE) MarshalJSON() ([]byte, error) {
+	return MarshalJSON_Not_Nulls(e)
+}
+
+const querySelectCoqSorteo = `select * from coq_sorteo_param_list( $1, $2)`
+
+//---------------------------------------------------------------------
+//MySQL               PostgreSQL            Oracle
+//=====               ==========            ======
+//WHERE col = ?       WHERE col = $1        WHERE col = :col
+//VALUES(?, ?, ?)     VALUES($1, $2, $3)    VALUES(:val1, :val2, :val3)
+//---------------------------------------------------------------------
+
+// GetAll returns a slice of all users, sorted by last name
+func (u *CoqSorteoE) GetAll(token string, filter string) ([]*CoqSorteoE, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := querySelectCoqSorteo
+
+	// Se deseenvuelve el JSON del Filter para adicionar filtros
+	var mapFilter map[string]interface{}
+	json.Unmarshal([]byte(filter), &mapFilter)
+	if mapFilter == nil {
+		mapFilter = make(map[string]interface{})
+	}
+	// --- Adicion de filtros
+	// mapFilter["tipo"] = tabla
+	// Se empaqueta el JSON del Filter
+	jsonFilter, err := json.Marshal(mapFilter)
+	if err != nil {
+		log.Println("Error convirtiendo el Filter")
+	}
+	log.Println("Where = " + string(jsonFilter))
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	rows, err := stmt.QueryContext(ctx, token, string(jsonFilter))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var lista []*CoqSorteoE
+
+	for rows.Next() {
+		var rowdata CoqSorteoE
+		err := rows.Scan(
+			&rowdata.Uniqueid,
+			//&rowdata.Owner,
+			//&rowdata.Dispositivoid,
+			&rowdata.Id,
+			&rowdata.Sede,
+			//&rowdata.Flag1,
+			//&rowdata.Flag2,
+			&rowdata.CountryCode,
+			&rowdata.Campeonatoid,
+			&rowdata.CondicionarPeso,
+			&rowdata.NoPelearMismoGalpon,
+			&rowdata.NoPelearMismoCriador,
+			&rowdata.PermitirPeleasMismoGrupo,
+			&rowdata.LimiteMaxDiffPeso,
+			&rowdata.PermitirGrabarConRezagados,
+			&rowdata.SecuenciaSorteo,
+			/*&rowdata.Ruf1,
+			&rowdata.Ruf2,
+			&rowdata.Ruf3,
+			&rowdata.Iv,
+			&rowdata.Salt,
+			&rowdata.Checksum,*/
+			&rowdata.FCreated,
+			&rowdata.FUpdated,
+			&rowdata.Activo,
+			&rowdata.Estadoreg,
+			&rowdata.TotalRecords,
+		)
+		if err != nil {
+			log.Println("Error scanning", err)
+			return nil, err
+		}
+
+		lista = append(lista, &rowdata)
+	}
+
+	return lista, nil
+}
+
+// GetOne returns one user by id
+func (u *CoqSorteoE) GetByUniqueid(token string, uniqueid int) (*CoqSorteoE, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := querySelectCoqSorteo
+
+	var rowdata CoqSorteoE
+	jsonText := fmt.Sprintf(`{"uniqueid":%d}`, uniqueid)
+	row := db.QueryRowContext(ctx, query, token, jsonText)
+
+	err := row.Scan(
+		&rowdata.Uniqueid,
+		//&rowdata.Owner,
+		//&rowdata.Dispositivoid,
+		&rowdata.Id,
+		&rowdata.Sede,
+		//&rowdata.Flag1,
+		//&rowdata.Flag2,
+		&rowdata.CountryCode,
+		&rowdata.Campeonatoid,
+		&rowdata.CondicionarPeso,
+		&rowdata.NoPelearMismoGalpon,
+		&rowdata.NoPelearMismoCriador,
+		&rowdata.PermitirPeleasMismoGrupo,
+		&rowdata.LimiteMaxDiffPeso,
+		&rowdata.PermitirGrabarConRezagados,
+		&rowdata.SecuenciaSorteo,
+		/*&rowdata.Ruf1,
+		&rowdata.Ruf2,
+		&rowdata.Ruf3,
+		&rowdata.Iv,
+		&rowdata.Salt,
+		&rowdata.Checksum,*/
+		&rowdata.FCreated,
+		&rowdata.FUpdated,
+		&rowdata.Activo,
+		&rowdata.Estadoreg,
+		&rowdata.TotalRecords,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &rowdata, nil
+}
+
+// Update updates one user in the database, using the information
+// stored in the receiver u
+func (u *CoqSorteoE) Update(token string, data string, metricas string) (map[string]any, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	// Se deseenvuelve el JSON del Data para adicionar filtros
+	var mapData map[string]interface{}
+	json.Unmarshal([]byte(data), &mapData)
+	if mapData == nil {
+		mapData = make(map[string]interface{})
+	}
+	// --- Adicion de filtro de tipos de carros
+	// mapData["tipo"] = tabla
+
+	// Se empaqueta el JSON del Data
+	jsonData, err := json.Marshal(mapData)
+	if err != nil {
+		log.Println("Error convirtiendo el Dato")
+		return nil, err
+	}
+	log.Println("Data = " + string(jsonData))
+
+	query := `SELECT coq_sorteo_param_save($1, $2, $3)`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := stmt.QueryContext(ctx, token, string(jsonData), metricas)
+	if err != nil {
+		return nil, err
+	}
+	defer result.Close()
+
+	var uniqueid int64
+
+	if result.Next() {
+		err := result.Scan(&uniqueid)
+		if err != nil {
+			log.Println("Error scanning", err)
+			return nil, err
+		}
+	}
+
+	retorno := make(map[string]any)
+	retorno["uniqueid"] = uniqueid
+
+	return retorno, nil
+}
+
+// Delete deletes one user from the database, by User.ID
+func (u *CoqSorteoE) Delete(token string, data string, metricas string) (map[string]any, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	// Se deseenvuelve el JSON del Data para adicionar filtros
+	var mapData map[string]interface{}
+	json.Unmarshal([]byte(data), &mapData)
+	if mapData == nil {
+		mapData = make(map[string]interface{})
+	}
+	// --- Adicion de estado de eliminacion de record
+	mapData["estadoreg"] = 300
+
+	// Se empaqueta el JSON del Data
+	jsonData, err := json.Marshal(mapData)
+	if err != nil {
+		log.Println("Error convirtiendo el Dato")
+		return nil, err
+	}
+	log.Println("Data = " + string(jsonData))
+
+	query := `SELECT coq_sorteo_param_save($1, $2, $3)`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := stmt.QueryContext(ctx, token, string(jsonData), metricas)
+	if err != nil {
+		return nil, err
+	}
+	defer result.Close()
+
+	var uniqueid int64
+
+	if result.Next() {
+		err := result.Scan(&uniqueid)
+		if err != nil {
+			log.Println("Error scanning", err)
+			return nil, err
+		}
+	}
+
+	retorno := make(map[string]any)
+	retorno["uniqueid"] = uniqueid
+
+	return retorno, nil
+}
+
+// DeleteByID deletes one user from the database, by ID
+func (u *CoqSorteoE) DeleteByID(token string, id int, orden int, metricas string) (map[string]any, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	jsonText := fmt.Sprintf(`{  "id":%d, 
+								"uniqueid":%d, 
+								"orden":%d, 
+								"estadoreg":%d
+							  }`,
+		id, id, orden, 300)
+
+	query := `SELECT coq_sorteo_param_save($1, $2, $3)`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := stmt.QueryContext(ctx, token, jsonText, metricas)
+	if err != nil {
+		return nil, err
+	}
+	defer result.Close()
+
+	var uniqueid int64
+
+	if result.Next() {
+		err := result.Scan(&uniqueid)
+		if err != nil {
+			log.Println("Error scanning", err)
+			return nil, err
+		}
+	}
+
+	retorno := make(map[string]any)
+	retorno["uniqueid"] = uniqueid
+
+	return retorno, nil
+}
